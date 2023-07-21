6446db30b35812b10297fa22bb4fcbae601afd2b
AlbertStoykov <albert.stoykov@gmail.com>
2023-07-21 19:34:15 +0100

    bugfix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,9 +15,12 @@ const Hero = () => {
       >
         GitHub
       </button>
-      <h1 className="head_text">ChatGPT Article Summarizer based on ChatGPT</h1>
+      <h1 className="head_text">
+        <span className="orange_gradient">SummarAIse</span>
+        <br></br>Article Summarizer based on ChatGPT
+      </h1>
       <h2 className="desc">
-        Tired of reading lenghty articles where 90% of the text is fluff? The
+        Tired of reading lenghty articles where 90% of the text is fluff? The{" "}
         <span className="orange_gradient">SummarAIse</span> encapsulator is here
         to help!
       </h2>
