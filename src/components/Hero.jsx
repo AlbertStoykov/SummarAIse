9e0b4d4e6dd69e441c579f6c3c3c1a936513549d
AlbertStoykov <albert.stoykov@gmail.com>
2023-07-21 12:50:02 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,17 @@ import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <header className="w-full flex justify-content items-center flex-col"></header>
+    <header className="w-full flex justify-content items-center flex-col">
+      <nav className="flex justify-between items-center flex-col">
+        <img src={logo} alt="sum_logo" className="w-28 object-contain" />
+      </nav>
+      <button
+        type="button"
+        onClick={() =>
+          window.open("https://github.com/AlbertStoykov/SummarAIse")
+        }
+      ></button>
+    </header>
   );
 };
 
