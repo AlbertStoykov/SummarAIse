a4864f1f57cf3d11e5c1328e900b27c19d586f23
AlbertStoykov <albert.stoykov@gmail.com>
2023-07-21 14:19:40 +0100

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -1,27 +1,32 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY
+const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
 
 const options = {
-    method: 'GET',
-    url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
-    params: {
-      url: 'https://time.com/6266679/musk-ai-open-letter/',
-      length: '3'
-    },
-    headers: {
-      'X-RapidAPI-Key': '35c19c5768msh536689eac30d811p1c33a3jsn05ad9b58ff09',
-      'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
-    }
-  };
+  method: "GET",
+  url: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize",
+  params: {
+    url: "https://time.com/6266679/musk-ai-open-letter/",
+    length: "3",
+  },
+  headers: {
+    "X-RapidAPI-Key": "35c19c5768msh536689eac30d811p1c33a3jsn05ad9b58ff09",
+    "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
+  },
+};
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
-prepareHeaders: (headers) => {
-    headers.set("X-RapidAPI-Key", "KEY")
-    headers.set("X-RapidAPI-Host", "article-extractor-and-summarizer.p.rapidapi.com")
-}})
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", rapidApiKey);
+      headers.set(
+        "X-RapidAPI-Host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
+    },
+  }),
   endpoints: (builder) => ({
     getSummary: builder.query({ query: (params) => "test" }),
   }),
