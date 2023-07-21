33513a9dff731b2f3cc771bea03d8e0616c72d20
AlbertStoykov <albert.stoykov@gmail.com>
2023-07-21 14:17:18 +0100

    KEY Variable

--- src/services/article.js
+++ src/services/article.js
@@ -1,5 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY
+
 const options = {
     method: 'GET',
     url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
