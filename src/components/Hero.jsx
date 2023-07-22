b82cb99a8b74e11640b68f1441896b8bb1342cd6
AlbertStoykov <albert.stoykov@gmail.com>
2023-07-22 21:23:07 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,22 +20,21 @@ const Hero = () => {
         </h2>
       </header>
 
-      <footer class="fixed bottom-0 left-0 w-full bg-white rounded-lg bg-transparent">
-        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-          <div class="sm:flex sm:items-center sm:justify-between"></div>
-          <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <span class="block text-lg text-black-500 sm:text-center dark:text-black-400">
+      <footer className="fixed bottom-0 left-0 w-full bg-white rounded-lg bg-transparent">
+        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <div className="sm:flex sm:items-center sm:justify-between"></div>
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <span className="block text-lg text-black-500 sm:text-center dark:text-black-400">
             © 2023
-            <a href="https://flowbite.com/" class="hover:underline">
-              SummarAIse™
+            <a href="https://flowbite.com/" className="hover:underline">
+              SummarAIse™. All Rights Reserved.
             </a>
-            . All Rights Reserved.
             <button
               type="button"
               onClick={() =>
                 window.open("https://github.com/AlbertStoykov/SummarAIse")
               }
-              className="black_btn pl-3"
+              className="black_btn m-5"
             >
               GitHub
             </button>
