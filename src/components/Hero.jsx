d17dbb58e1fa34b97b253c050d5a2195a105ec0c
AlbertStoykov <albert.stoykov@gmail.com>
2023-07-22 22:08:18 +0100

    Github Button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,16 +26,23 @@ const Hero = () => {
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span className="block text-lg text-black-500 sm:text-center dark:text-black-400">
             © 2023 SummarAIse
-            <button
-              type="button"
-              onClick={() =>
-                window.open("https://github.com/AlbertStoykov/SummarAIse")
-              }
-              className="black_btn m-10"
+            {/* Github Button */}
+            <a
+              href="https://github.com/AlbertStoykov/SummarAIse"
+              className="relative inline-block text-lg group m-12"
             >
-              GitHub
-            </button>
+              <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                <span className="relative">GitHub</span>
+              </span>
+              <span
+                className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                data-rounded="rounded-lg"
+              ></span>
+            </a>
           </span>
+          {/* Github Button */}
         </div>
       </footer>
     </>
