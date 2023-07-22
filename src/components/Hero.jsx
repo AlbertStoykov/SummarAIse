7c7cfb8c3b04236c228178f1acf485dbbcd71025
AlbertStoykov <albert.stoykov@gmail.com>
2023-07-22 21:57:53 +0100

    Styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <>
       <header className="w-full flex justify-content items-center flex-col">
         <nav className="flex justify-between w-full mb-10 pt-3">
-          <img src={logo} alt="sum_logo" className="w-[25%] object-contain" />
+          <img src={logo} alt="sum_logo" className="w-[30%] object-contain" />
         </nav>
         <h1 className="head_text">
           <i>SummarAIse</i>
@@ -21,7 +21,7 @@ const Hero = () => {
         </h2>
       </header>
 
-      <footer className="fixed bottom-0 left-0 w-full">
+      <footer className="fixed bottom-0 left-0 w-full text-white">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span className="block text-lg text-black-500 sm:text-center dark:text-black-400">
